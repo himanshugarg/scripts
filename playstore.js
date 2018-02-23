b80d9df9338d9f83200401837884ca91f105d6db
Himanshu <himanshugarg@zoho.com>
2018-02-23 18:39:47 +0530

    wip: capture details from details pages

--- playstore.js
+++ playstore.js
@@ -1,5 +1,23 @@
-jQuery('.details').each(function() {console.log(
-  '"' + $.trim($(this).find('.title').text()) 
-  + '","' + $.trim($(this).find('.subtitle').text()) 
-  + '",' + /Free|\d+(\.\d+)?/.exec($.trim($(this).find('.display-price').text()))[0] 
-  + ',' + ($(this).parent().find('.tiny-star').length && /\d+(\.\d+)+/.exec($.trim($(this).parent().find('.tiny-star').attr('aria-label')))[0]) || '')});
+var r = {};
+jQuery('.details').each(function() {
+    var link = $.trim($(this).find('.title').attr('href'));
+    r[link].push('"' + $.trim($(this).find('.title').text()) + '"');       // name
+    r[link].push('"' + link + '"'); // details page link
+    r[link].push('"' + $.trim($(this).find('.subtitle').text()) + '"');    // company
+    r[link].push(/Free|\d+(\.\d+)?/.exec($.trim($(this).find('.display-price').text()))[0]);
+    var rating = $(this).parent().find('.tiny-star');                // star rating
+    r[link].push((rating.length && /\d+(\.\d+)+/.exec(rating.attr('aria-label'))[0]) || '');
+});
+for (var link in r) {
+    jQuery.get(link, function(html) {
+	jQuery.each(jQuery.parseHTML(html), function(j, e) {
+	    jQuery(e).find('.meta-info > .content').each(function() {
+		var txt = jQuery.trim(jQuery(this).text().replace(/\s+/gm, ' '));
+		r[link].push('"'+txt+'"');
+	    });
+	});
+	console.log(r[link].join(','));
+    });
+}
+
+
