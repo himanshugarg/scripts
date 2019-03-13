c95b18f19057ac4e06edf4b35db8c8e48a94220e
Himanshu <hgarg@acm.org>
2019-03-13 15:41:43 +0530

    Update cactus.js

--- cactus.js
+++ cactus.js
@@ -1,7 +1,7 @@
 let shown_trs = document.getElementsByClassName('mrh-detail-row');
 for (let i = 0; i < shown_trs.length; i++) {
   let tds = shown_trs[i].getElementsByTagName('td');
-  let m = tds[4].textContent.match(/Unit count: (\d\d\d?)\s/);
+  let m = tds.length < 5?"":tds[4].textContent.match(/Unit count: (\d\d\d?)\s/);
   // hide rows with more than 800 words
   if (!m || parseInt(m[1]) > 800) shown_trs[i].style.display = "none";
 }
