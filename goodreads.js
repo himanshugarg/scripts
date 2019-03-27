96f5f53a1c5eabda4eb49e82a5df63ecaf50f52e
Himanshu <hgarg@acm.org>
2019-03-28 04:37:07 +0530

    Update goodreads.js
    updated for new markup

--- goodreads.js
+++ goodreads.js
@@ -1,4 +1,4 @@
-jQuery('.minirating').each(function() { 
-    if (!jQuery(this).text().match(/\d\d\d,\d\d\d/))
-        jQuery(this).closest('tr').hide();
+jQuery('.smallText.greyText').each(function() { 
+    if (!jQuery(this).text().match(/\d\d,\d\d\d ratings/))
+        jQuery(this).closest('div').parent().hide();
 });
